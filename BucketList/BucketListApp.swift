9d6d8fb5926772491df29f0ffa268e4dbe7b4361
Jacob LeCoq <lecoqjacob@gmail.com>
2021-02-25 14:24:36 -0600

    init

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Jacob LeCoq on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Jacob LeCoq on 2/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
